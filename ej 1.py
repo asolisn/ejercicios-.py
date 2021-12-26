5401512de32568ea3fb3818cac2c017491a0f144
asolisn <85768831+asolisn@users.noreply.github.com>
2021-12-26 16:10:50 -0500

    Add files via upload

---
+++ ej 1.py
@@ -0,0 +1,14 @@
+variable="Hola Mundo"
+variable1=True
+variable2= " "
+variable3=",,1"
+variable4=",,c´"
+variable5=256
+variable6=8>19
+print(type(variable))
+print(type(variable1))
+print(type(variable2))
+print(type(variable3))
+print(type(variable4))
+print(type(variable5))
+print(type(variable6))
